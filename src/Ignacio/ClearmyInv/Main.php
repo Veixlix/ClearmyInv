434595a0c02bc30c21a84d12452087867fce31c0
Ignacio <45845856+Ignaciox@users.noreply.github.com>
2020-06-05 08:16:25 +0200

    Initial commit

---
+++ src/Ignacio/ClearmyInv/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ignacio\ClearmyInv;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+
+	
+public function onJoin(PlayerJoinEvent $ev){
+	$ev->getPlayer()->getInventory()->clearAll();
+	
+	 }
+	  }
